9f38c240cb30fb6688c1b30b76f8a61676874711
Eya Kadri <eyakaad@gmail.com>
2025-03-13 14:35:32 -0500

    0-validate_utf8.py

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a method that determines if a given data set represents a valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    """a method that determines if a given data set represents a valid UTF-8 encoding"""
+    try:
+        maskeddata = [n & 255 for n in data]
+        bytes(maskeddata).decode("UTF-8")
+        return True
+    except Exception:
+        return False
