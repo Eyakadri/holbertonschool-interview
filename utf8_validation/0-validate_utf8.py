dbc2274c104b5b0766ba2a458ddab1a1a062490d
Eya Kadri <eyakaad@gmail.com>
2025-03-13 14:37:22 -0500

    0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""a method that determines if a given data set represents a valid UTF-8 encoding"""
+"""a method that determines if a given data"""
 
 
 def validUTF8(data):
-    """a method that determines if a given data set represents a valid UTF-8 encoding"""
+    """a method that determines if a given data """
     try:
         maskeddata = [n & 255 for n in data]
         bytes(maskeddata).decode("UTF-8")
