78ff438c1a6e9aae14266a62c84fb2ab790a9a3a
Eya Kadri <eyakaad@gmail.com>
2025-03-13 14:46:15 -0500

    0-minoperations.py

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+
+def minOperations(n):
+    """ Minimum Operations """
+    if not isinstance(n, int) or n < 2:
+        return 0
+    op = 0
+    k = 3
+    while n % 2 == 0:
+        op += 2
+        n = n//2
+    while k <= n:
+        while n % k == 0:
+            op += k
+            n = n//k
+        k += 2
+    return int(op)
