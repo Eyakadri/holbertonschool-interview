f79af5b4ed7a2b83cf97b4bdb21d5b49347934fc
Eyakadri <eyaakadrii@gmail.com>
2025-06-21 11:51:51 +0100

    0-making_change.py

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 
-""" making change fun"""
+""" making change fun """
 
 
 def makeChange(coins, total):
     """
-    returns: fewest number of coins needed to meet total
+    Returns: fewest number of coins needed to meet total
     If total is 0 or less, return 0
     If total cannot be met by any number of coins you have, return -1
     """
-    if not coins or coins is None:
-        return -1
     if total <= 0:
         return 0
-    change = 0
-    coins = sorted(coins)[::-1]
+    if not coins:
+        return -1
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
     for coin in coins:
-        while coin <= total:
-            total -= coin
-            change += 1
-        if (total == 0):
-            return change
-    return -1
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
